137d385ad4c6290f549f7c89d0bf8654aaf64d03
Mustapha Nkhili <mustapha.developer.23@gmail.com>
2026-07-22 18:28:20 +0100

    fix(coder): check simulation stop status in coder_routine loop

--- src/coder.c
+++ src/coder.c
@@ -6,7 +6,7 @@
 /*   By: mn-khili <mn-khili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 21:41:56 by mn-khili          #+#    #+#             */
-/*   Updated: 2026/07/22 04:36:37 by mn-khili         ###   ########.fr       */
+/*   Updated: 2026/07/22 18:21:40 by mn-khili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,24 @@
 #include <unistd.h>
 #include "codexion.h"
 
-static void	compile_phase(t_coder_args *arg, struct s_params *params)
+static int	compile_phase(t_coder_args *arg, struct s_params *params)
 {
 	arg->coder->state = STATE_WAITING_FOR_DONGLES;
+	if (is_sim_should_stop(arg->sim_state))
+		return (1);
 	acquire_both_dongles(arg->coder, arg->dongles, arg->ticket_counter,
 		arg->logger);
 	arg->coder->last_compile_start = get_timestamp_ms();
 	arg->coder->state = STATE_COMPILING;
+	if (is_sim_should_stop(arg->sim_state))
+		return (1);
 	log_state(arg->logger, arg->coder->id, STATE_COMPILING);
 	usleep(params->time_to_compile * 1000);
 	release_both_dongles(arg->coder, arg->dongles, params->dongle_cooldown);
 	arg->coder->compile_count++;
 	if (arg->coder->compile_count == params->number_of_compiles_required)
 		mark_coder_finished_sim(arg->sim_state);
+	return (0);
 }
 
 void	*coder_routine(void *a)
@@ -40,11 +45,16 @@ void	*coder_routine(void *a)
 	{
 		if (arg->coder->compile_count >= params->number_of_compiles_required)
 			break ;
-		compile_phase(arg, params);
+		if (compile_phase(arg, params))
+			break ;
 		arg->coder->state = STATE_DEBUGGING;
+		if (is_sim_should_stop(arg->sim_state))
+			break ;
 		log_state(arg->logger, arg->coder->id, STATE_DEBUGGING);
 		usleep(params->time_to_debug * 1000);
 		arg->coder->state = STATE_REFACTORING;
+		if (is_sim_should_stop(arg->sim_state))
+			break ;
 		log_state(arg->logger, arg->coder->id, STATE_REFACTORING);
 		usleep(params->time_to_refactor * 1000);
 	}
